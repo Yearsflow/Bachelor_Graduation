f0bb6e13e45689e1a9be9a77e77aa214f51bf859
Yearsflow <yearsflow@126.com>
2022-01-20 22:25:02 +0800

    config

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,flash,redirect,url_for
+from flask_login import current_user,login_user,logout_user,login_required
+from markupsafe import escape
 
 app=Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if isinstance(current_user._get_current_object(),Administrator):
+        logout_user()
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:tyc0520!@localhost:3306/bachelor_graduation?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
