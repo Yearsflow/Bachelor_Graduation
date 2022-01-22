bc3fb0bf8855dd45f21cf737317fec35fa8e808e
Yearsflow <yearsflow@126.com>
2022-01-22 16:47:42 +0800

    login

--- Bachelor_Graduation.sql
+++ Bachelor_Graduation.sql
@@ -10,3 +10,17 @@ create table administrator (
 lock tables administrator write;
 insert into administrator values ('0000','admin','admin@tust.edu.cn','admin');
 unlock tables;
+
+drop table if exists users;
+create table users (
+	Id varchar(8) collate utf8_unicode_ci not null,
+	Name varchar(8) collate utf8_unicode_ci not null,
+	Email varchar(20) collate utf8_unicode_ci not null,
+	Password text collate utf8_unicode_ci not null,
+	primary key (Id)
+) ENGINE=InnoDB default charset=utf8 collate=utf8_unicode_ci;
+
+lock tables users write;
+insert into users values ('0000','test','test@tust.edu.cn','123456');
+insert into users values ('0001','DCrusher','yearsflow@126.com','123456');
+unlock tables;
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,flash,redirect,url_for
 from flask_login import current_user,login_user,logout_user,login_required
+from importlib_metadata import re
 from markupsafe import escape
 
 app=Flask(__name__)
@@ -14,8 +15,12 @@ def show_user_profile(username):
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if isinstance(current_user._get_current_object(),Administrator):
-        logout_user()
+    if request.method=='GET':
+        return render_template('login.html')
+    if request.method=='POST':
+        print(request.form.get('logemail'))
+        print(request.form.get('logpass'))
+        return 'this is post'
 
 if __name__=='__main__':
     app.run(debug=True)
