e98ef503046ca883276b3fa366e204abab9f3c66
Yearsflow <yearsflow@126.com>
2022-01-25 21:22:40 +0800

    config

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-import os
-
 class Config(object):
     SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:tyc0520!@localhost:3306/bachelor_graduation?charset=utf8'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
