20daab5cc16a0ff86c8e8917a23748f30c5a531b
Yearsflow <yearsflow@126.com>
2022-01-26 22:58:11 +0800

    logs

--- Bachelor_Graduation.sql
+++ Bachelor_Graduation.sql
@@ -1,6 +1,6 @@
 drop table if exists administrator;
 create table administrator (
-	Id varchar(8) collate utf8_unicode_ci not null,
+	Id int(11) collate utf8_unicode_ci not null auto_increment,
 	Name varchar(10) collate utf8_unicode_ci not null,
 	Email varchar(20) collate utf8_unicode_ci not null,
 	Password text collate utf8_unicode_ci not null,
@@ -8,12 +8,12 @@ create table administrator (
 ) ENGINE=InnoDB default charset=utf8 collate=utf8_unicode_ci;
 
 lock tables administrator write;
-insert into administrator values ('0000','admin','admin@tust.edu.cn','admin');
+insert into administrator values (1,'admin','admin@tust.edu.cn','admin');
 unlock tables;
 
 drop table if exists users;
 create table users (
-	Id varchar(8) collate utf8_unicode_ci not null,
+	Id int(11) collate utf8_unicode_ci not null auto_increment,
 	Name varchar(8) collate utf8_unicode_ci not null,
 	Email varchar(20) collate utf8_unicode_ci not null,
 	Password text collate utf8_unicode_ci not null,
@@ -21,6 +21,6 @@ create table users (
 ) ENGINE=InnoDB default charset=utf8 collate=utf8_unicode_ci;
 
 lock tables users write;
-insert into users values ('0000','test','test@tust.edu.cn','123456');
-insert into users values ('0001','DCrusher','yearsflow@126.com','123456');
+insert into users values (1,'test','test@tust.edu.cn','123456');
+insert into users values (2,'DCrusher','yearsflow@126.com','123456');
 unlock tables;
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bachelor_Graduation
 TUST本科毕业设计（基于机器学习的新闻标题分类系统）
+
+chinese_bert_base预训练模型训练train&dev&test句向量数据集在下面百度网盘链接，有需要的看客自取~
+
+链接：https://pan.baidu.com/s/11aCCEZ1XaYa6z4Mne9qx6Q 
+提取码：8m01 
+
